37ee555764450c0b66f71f703cca5ba45b359891
Mayur Sonawane <mayur.sonawanemds12@gmail.com>
2025-07-30 13:36:04 +0530

    Third ommit

--- BE/models/User.js
+++ BE/models/User.js
@@ -1,12 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  phone: { type: String },
-  address: { type: String }
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
 
-export default mongoose.model('User', userSchema); 
+export default  mongoose.model("userExpense",UserSchema);
--- BE/server.js
+++ BE/server.js
@@ -3,34 +3,27 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+// // File Import
+import authRoutes from './routes/authRoutes.js';
+import transactionRoutes from './routes/transactionRoutes.js';
 
-//File import
-import userRoutes from './routes/userRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-import wishlistRoutes from './routes/wishlistRoutes.js';
- 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//Routes
-app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/wishlist', wishlistRoutes);
+// Routes 
+app.use('/api/auth' , authRoutes);
+app.use('/api/transactions' , transactionRoutes);
 
-//DB connection
+// DB connection 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
-    console.log("---------DB Connected ...!------------");
-})
-.catch((err)=>{
-    console.log(`DB cannot be connected,${err}`);
+    console.log("------MongoDB Connected ...!---------");
     
 })
-
-app.listen(5000);
+.catch((err)=>{
+    console.log(`DB connection Error, ${err}`);
+    
+})
+app.listen(7000);
